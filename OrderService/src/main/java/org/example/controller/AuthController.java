bac4985f35fd46acf4fbf4e3a0d86eb0b756ba6b
RocketBunny <guzenkovsk@gmail.com>
2025-04-13 17:57:49 +0700

    add auth service (earlier it`s logic were inside customer service)

--- OrderService/src/main/java/org/example/controller/AuthController.java
+++ OrderService/src/main/java/org/example/controller/AuthController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.dto.AuthRequest;
 import org.example.dto.AuthResponse;
 import org.example.dto.SignupResponse;
+import org.example.service.AuthService;
 import org.example.service.CustomerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
 
-    private final CustomerService customerService;
+    private final AuthService authService;
 
     @PostMapping("/signup")
     public ResponseEntity<SignupResponse> signup(@RequestBody AuthRequest authRequest) {
-        SignupResponse response = customerService.signup(authRequest);
+        SignupResponse response = authService.signup(authRequest);
 
         if (response.getMessage().equals("User already exists")) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
@@ -33,7 +34,7 @@ public class AuthController {
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthRequest request) {
         try {
-            AuthResponse response = customerService.login(request);
+            AuthResponse response = authService.login(request);
             return ResponseEntity.ok(response);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
--- OrderService/src/main/java/org/example/service/CustomerService.java
+++ OrderService/src/main/java/org/example/service/CustomerService.java
@@ -2,21 +2,13 @@ package org.example.service;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
-import org.example.dto.AuthRequest;
-import org.example.dto.AuthResponse;
 import org.example.dto.CustomerRequest;
-import org.example.dto.SignupResponse;
 import org.example.model.Address;
 import org.example.model.AuthUser;
 import org.example.model.Customer;
-import org.example.model.Token;
 import org.example.repository.IAddressRepository;
 import org.example.repository.IAuthUserRepository;
 import org.example.repository.ICustomerRepository;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -28,44 +20,6 @@ public class CustomerService {
     private final ICustomerRepository customerRepository;
     private final IAuthUserRepository authUserRepository;
     private final IAddressRepository addressRepository;
-    private final JwtTokenService tokenService;
-    private final AuthenticationManager authenticationManager;
-    private final PasswordEncoder passwordEncoder;
-
-    public SignupResponse signup(AuthRequest authRequest) {
-        if (authUserRepository.findByLogin(authRequest.getLogin()).isPresent()) {
-            return new SignupResponse("User already exists");
-        }
-
-        AuthUser newUser = AuthUser.builder()
-                .login(authRequest.getLogin())
-                .password(passwordEncoder.encode(authRequest.getPassword()))
-                .userType(authRequest.getUserType())
-                .customer(authRequest.getCustomer())
-                .build();
-
-        addAuthUser(newUser);
-
-        return new SignupResponse("User created successfully");
-    }
-
-    public AuthResponse login(AuthRequest request) {
-        authenticationManager.authenticate(
-                new UsernamePasswordAuthenticationToken(request.getLogin(), request.getPassword())
-        );
-
-        AuthUser user = authUserRepository.findByLogin(request.getLogin())
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
-        Token token = tokenService.generateToken(user);
-        return new AuthResponse(token.getToken());
-    }
-
-    public AuthUser addAuthUser(AuthUser authUser) {
-        Customer customer = addNewCustomerIfNotExists(authUser.getCustomer());
-        authUser.setCustomer(customer);
-        return authUserRepository.save(authUser);
-    }
 
     public Optional<Customer> findCustomerByLogin(String login) {
         return authUserRepository.findByLogin(login)
@@ -80,7 +34,7 @@ public class CustomerService {
         return customerRepository.findAll();
     }
 
-    private Customer addNewCustomerIfNotExists(Customer customer) {
+    public Customer addNewCustomerIfNotExists(Customer customer) {
         Optional<Customer> existingCustomer = customerRepository.findByName(customer.getName());
 
         if (existingCustomer.isPresent()) {
